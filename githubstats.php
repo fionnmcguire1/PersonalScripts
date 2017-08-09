6474d4cc51d092b6b3c845e1cc873ee600db5189
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-08-09 23:16:49 +0100

    JSON structure wrong in issues

--- githubstats.php
+++ githubstats.php
@@ -8,8 +8,8 @@ Description:
 
 */
 date_default_timezone_set('Europe/Dublin');
-
-require_once('../github-php-client-master/client/GitHubClient.php');
+$auth_token = "a97d0a92a462c6fa41cb0f125adaf90909761258";
+//require_once('../github-php-client-master/client/GitHubClient.php');
 
 /*foreach($argv as $value)
 {
@@ -19,9 +19,9 @@ print "***  Welcome to Gitstats CL Admin Client  ***\n\n
 Gitstats is a very simple script used to get information about\n
 your github account and individual github repositories\n\n";
 $uname = readline("Enter Username: ");
-$pass = readline("Enter Password: ");
-$client = new GitHubClient();
-$client->setCredentials($uname, $pass);
+//$pass = readline("Enter Password: ");
+//$client = new GitHubClient();
+//$client->setCredentials($uname, $pass);
 $withbase = false;
 $allData = json_decode(get_json("/users/$uname/repos",$uname,$withbase), true);
 
@@ -29,41 +29,67 @@ $response = true;
 while($response != false)
 {
   $command = readline("Enter Command: ");
+  $commandArguments = explode(" ", $command);
 
-  if(substr($command, 0, 5) == 'rlist')
+  if($commandArguments[0] == 'rlist')
   {
     $i = 0;
     while($i < count($allData))
     {
       echo "------> ".$allData[$i]['name']."\n";
+      print "";
       $i+=1;
     }
   }
-  else if(substr($command, 0, 6) == 'rstats')
+  else if($commandArguments[0] == 'rstats')
   {
     if(strlen($command) > 6)
     {
-        $repo = trim(substr($command, 6));
+        $repo = $commandArguments[1];
         $i = 0;
         while($i < count($allData))
         {
           if($allData[$i]['name'] == $repo)
           {
             $commitsData = json_decode(get_json($allData[$i]['commits_url'],$uname,true), true);
+            $OpenIssuesData = json_decode(get_json($allData[$i]['issues_url'],$uname,true), true);
+
             print "-----> Commits: ".count($commitsData)."\n";
-            print "-----> Last Commit: ".ConvertToReadableTimePast(substr($commitsData[0]['committer']['date'], 0, 10))."\n";
-            print "-----> Number of Files: ".$allData[$i]['size']."\n";
+            //print "-----> Last Commit: ".ConvertToReadableTimePast(substr($commitsData[0]['committer']['date'], 0, 10))."\n";
+            print "-----> Size: ".$allData[$i]['size']."KB \n";
             print "-----> Issue Count: ".$allData[$i]['open_issues_count']."\n";
             print "-----> Dev Time: ".((strtotime(date('Y-m-d')) - strtotime(substr($allData[$i]['created_at'], 0, 10)))/ (86400))." Days \n";
-
-            break 2;
+            print "";
+            break 1;
           }
           $i+=1;
         }
     }
 
   }
-  else if($command == 'q' || $command == 'Q' || $command == 'quit')
+  else if($commandArguments[0] == 'issues')
+  {
+    $repo = $commandArguments[1];
+    //print $repo;
+    $i = 0;
+    while($i < count($allData))
+    {
+      if($allData[$i]['name'] == $repo)
+      {
+        $OpenIssuesData = json_decode(get_json($allData[$i]['issue_events_url'],$uname,true), true);
+        $j = 0;
+        while($j < count($OpenIssuesData))
+        {
+          print "-----> Title: ".$OpenIssuesData[$j]['title']."\n";
+          print "-----> Body: ".$OpenIssuesData[$j]['body']."\n";
+          print "";
+          $j+=1;
+        }
+      }
+      $i+=1;
+    }
+  }
+  else if($commandArguments[0] == 'q' || $commandArguments[0] == 'Q' || $commandArguments[0] == 'quit')
   {
     $response = false;
   }
