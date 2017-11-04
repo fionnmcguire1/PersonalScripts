19c431523adee427ee87f89341012f6c9bdffb39
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-11-04 22:54:36 +0000

    New gc command added

--- bashAliasCommands.sh
+++ bashAliasCommands.sh
@@ -11,15 +11,29 @@ then
 	killall Finder
 elif [ $1 == "gc" ]
 then
+	$counter = 0
+	$currentPath = pwd
 	while [ 1 ]
 	do	
 		if [ -d .git ]
 		then
 			git add -A
-			git commit -m $2
+			git commit -m "$2"
+			cd $currentPath
 			break
 		else
-			git rev-parse --git-dir 2> /dev/null;
+			$counter = $counter+1
+			cd ../
+		fi
+		#Never going to have 3 or more nested folders 
+		#Primarily because it's overkill for anything thats not web dev
+		#This is basically to stop infinite loops
+		if [ $counter >= 3 ]
+		then
+			echo "This alias doesn't allow for commits on 3 or more nested folders\n 
+			Use: user$ git add -A\n
+			user$ git commit -m \"Message for commit! \""
+			break
 		fi
 	done
 fi
